c480f2ae4cb5e972ce1eb3cb177141f584e38d0d
uvpatel <uvpatel7271@gmail.com>
2026-04-04 18:11:52 +0530

    install dependency

--- server/app.py
+++ server/app.py
@@ -36,7 +36,7 @@ except Exception as e:  # pragma: no cover
     ) from e
 
 try:
-    from ..models import PythonAction, PythonObservation
+    from models import PythonAction, PythonObservation
     from .python_env_environment import PythonEnvironment
 except ModuleNotFoundError:
     from models import PythonAction, PythonObservation
